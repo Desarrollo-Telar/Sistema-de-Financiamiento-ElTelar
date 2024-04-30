e7a69bb09d89605833af87c28bf6da876e78d6dc
choc_1403 <eloicx@gmail.com>
2024-04-30 11:57:11 -0600

    Apartado de usuario y rol

--- Backend/project/apps/users/models.py
+++ Backend/project/apps/users/models.py
@@ -5,6 +5,8 @@ from datetime import datetime
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
+from apps.roles.models import Role
+
 # Creacion de todas las tablas para este modulo
 
 # Creacion de tabla de usuario
@@ -29,7 +31,23 @@ class User(AbstractUser):
     profile_pic = models.ImageField(upload_to='users/profile_pics/')
     creation_date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        if(self.first_name==''):
+            return '{}'.format(self.username)
+        return '{} {}'.format(self.first_name, self.last_name)
+    
+    def get_full_name(self):
+        if(self.first_name==''):
+            return '{}'.format(self.username)
+        return '{} {}'.format(self.first_name, self.last_name)
 
+
+class UserRole(models.Model):
+    idUser = models.ForeignKey(User, blank=False, null=False,on_delete=models.CASCADE)
+    idRole = models.ForeignKey(Role, blank=False, null=False, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'Rol de {self.idRole} para {self.idUser}'
 
 @receiver(pre_save, sender=User)
 def set_user_code(sender, instance, *args, **kwargs):
