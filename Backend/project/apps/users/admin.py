b782292c1028dcf2d304870a95284219d53105dc
choc_1403 <eloicx@gmail.com>
2024-04-30 12:08:49 -0600

    Modificacion en el apartado del admin de usuarios

--- Backend/project/apps/users/admin.py
+++ Backend/project/apps/users/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import User, UserRole
 # Register your models here.
-admin.site.register(User)
+
 admin.site.register(UserRole)
+
+class UserAdmin(admin.ModelAdmin):
+    fields = ('first_name','last_name', 'username', 'email', 'password','type_identification','identification_number','telephone','status','gender','nationality','profile_pic')
+    list_display = ('__str__', 'user_code','creation_date')
+admin.site.register(User, UserAdmin)
