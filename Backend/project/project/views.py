e0306aeb13df644d516813c9a02052dd439bb197
choc_1403 <eloicx@gmail.com>
2024-05-01 11:04:26 -0600

    Cerrar sesion

--- Backend/project/project/views.py
+++ Backend/project/project/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def prueba(request):
     enviar_correo('Mensaje de prueba', 'Esto solo es una prueba', 'eloicx@gmail.com')
+
+### -- APARTADO DE SALIR --##
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Sesión cerrada exitosamente')
+    return redirect('login')
