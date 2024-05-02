7e3c10201630d61df1ec78a1a9ddf7ba3ea916e3
choc_1403 <eloicx@gmail.com>
2024-05-02 14:32:32 -0600

    Cambio en la estructura de la tabla de roles

--- Backend/project/apps/roles/models.py
+++ Backend/project/apps/roles/models.py
@@ -1,15 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import Permission
 # Create your models here.
 class Role(models.Model):
-    roles = [
-        ('Administrador', 'Administrador'),
-        ('Contabilidad', 'Contabilidad'),
-        ('Secretaria', 'Secretaria'),
-        ('Programador', 'Programador')
-    ]
-    role_name = models.CharField(choices=roles, max_length=75, unique=True)
+    role_name = models.CharField(max_length=100, unique=True)
+    permissions = models.ManyToManyField('auth.Permission', blank=True)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.role_name
+
+
+
+class Meta:
+
+    permissions = [
+        ("agregar_usuario", "Tiene permiso de poder agregar un usuario al sistema"),
+        ("editar_usuario", "Tiene permiso para editar un usuario del sistema"),
+        ("eliminar_usuario","Tiene permiso puede eliminar un usuario del sistema")
+    ]
