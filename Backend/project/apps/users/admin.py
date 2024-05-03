6c1fd5e9edcc93a8d20b4c99e4c55f981df0ea8f
choc_1403 <eloicx@gmail.com>
2024-05-03 15:14:08 -0600

    Vistas para la generacion de codigos de verificacion

--- Backend/project/apps/users/admin.py
+++ Backend/project/apps/users/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 from .models import User, UserRole
+from .models import VerificationToken
 # Register your models here.
 
 admin.site.register(UserRole)
+admin.site.register(VerificationToken)
 
 class UserAdmin(admin.ModelAdmin):
     fields = ('first_name','last_name', 'username', 'email', 'password','type_identification','identification_number','telephone','status','gender','nationality','profile_pic')
--- Backend/project/apps/users/views.py
+++ Backend/project/apps/users/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from django.http import HttpResponse
+from .models import VerificationToken, User
+from twilio.rest import Client
+
+
+# Vista para la generacion de codigo
+@login_required
+def send_verification_code(request):
+    user = request.user
+    token = VerificationToken.generate_token(user)
+    # Aquí puedes enviar el código por correo electrónico, SMS, etc.
+    return HttpResponse(f'Se ha enviado el código de verificación al usuario {user.username}.')
+
+@login_required
+def send_verification_code(request):
+    user = request.user
+    token = VerificationToken.generate_token(user)
+    
+    # Configuración de Twilio
+    account_sid = 'tu_account_sid_de_twilio'
+    auth_token = 'tu_auth_token_de_twilio'
+    client = Client(account_sid, auth_token)
+
+    # Enviar código de verificación por SMS
+    message = client.messages.create(
+        body=f'Tu código de verificación es: {token.code}',
+        from_='tu_numero_de_twilio',
+        to='numero_del_usuario'
+    )
+
+    # Enviar código de verificación por WhatsApp
+    whatsapp_message = client.messages.create(
+        body=f'Tu código de verificación es: {token.code}',
+        from_=f'whatsapp:+502 42249955',
+        to=f'whatsapp:+502 {user.telephone}'
+    )
+
+    return HttpResponse(f'Se ha enviado el código de verificación al usuario {user.username} por SMS y WhatsApp.')
