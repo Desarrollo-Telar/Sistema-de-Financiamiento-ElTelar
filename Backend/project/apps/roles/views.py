1b0f43d4868cd77d88e89bd0b06535221b384d6d
choc_1403 <eloicx@gmail.com>
2024-05-14 10:11:36 -0600

    Descripcion del rol y permisos

--- Backend/project/apps/roles/views.py
+++ Backend/project/apps/roles/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 
+# Decorador
 from django.contrib.auth.decorators import login_required
+
+# Modelo
+from .models import Role
 
 # Create your views here.
 @login_required
 def role_permission(request):
     template_name = 'role&permission/index.html'
+    role = Role.objects.all()
     context = {
-        'title': 'EL TELAR - ROLES Y PERMISOS'
+        'title': 'EL TELAR - ROLES Y PERMISOS',
+        'role_list':role,
     }
     return render(request, template_name, context)
