823aaa00f62184226d143d202f469bf3ada5e994
choc_1403 <eloicx@gmail.com>
2024-05-20 15:10:54 -0600

    Inicio de relaciones

--- Backend/project/apps/InvestmentPlan/models.py
+++ Backend/project/apps/InvestmentPlan/models.py
@@ -9,11 +9,11 @@ class InvestmentPlan(models.Model):
         ('Local', 'Local'),
         ('Internacional', 'Internacional')
     ]
-    type_of_product_or_service # Tipo de producto o servicio
-    total_value_of_the_product_or_service # Valor total del producto o servicio
+    type_of_product_or_service = models.CharField(max_length=75, blank=False, null=False) # Tipo de producto o servicio
+    total_value_of_the_product_or_service = models.CharField(max_length=75, blank=False, null=False) # Valor total del producto o servicio
     investment_plan_description = models.TextField(blank=True, null=True) # Descripción del plan de inversión
     initial_amount  = models.CharField(max_length=75, blank=False, null=False) # Monto Inicial a manejar en el producto o servicios
     monthly_amount = models.CharField(max_length=75, blank=False, null=False) # Monto Mensual a manejar en el producto o servicios
     transfers_or_transfer_of_funds = models.BooleanField(blank=False, null=False) # transferencias o traslado de fondos Si o No
-    type_of_transfers_or_transfer_of_funds # tipo de transferencias o traslado de fondos Local o Internacional
+    type_of_transfers_or_transfer_of_funds  = models.CharField(max_length=75, choices=type_of_transfers_or_transfer_of_funds)# tipo de transferencias o traslado de fondos Local o Internacional
     customer_id = models.ForeignKey(Customer, blank=False, null=False, on_delete=models.CASCADE)
