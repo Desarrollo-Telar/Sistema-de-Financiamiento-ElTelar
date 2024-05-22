749f04d901c6790971a470c81a3ef588ba3e28c3
choc_1403 <eloicx@gmail.com>
2024-05-22 10:37:55 -0600

    Paginacion

--- Backend/project/apps/customers/views.py
+++ Backend/project/apps/customers/views.py
@@ -6,11 +6,18 @@ from .models import Customer
 # Decoradores
 from django.contrib.auth.decorators import login_required
 
+# Paginacion
+from project.pagination import paginacion
+
 # Create your views here.
 @login_required
 def list_customer(request):
+    customer_list = Customer.objects.all().order_by('-id')
+    page_obj = paginacion(request, customer_list)
     template_name = 'customer/list.html'
     context = {
         'title':'EL TELAR - CLIENTES',
+        'page_obj':page_obj,
+        'customer_list':customer_list,
     }
     return render(request, template_name, context)
