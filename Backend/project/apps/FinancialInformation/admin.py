4336cd977a909083e119132ada8e9474e3a9c80c
choc_1403 <eloicx@gmail.com>
2024-05-22 09:24:23 -0600

    Registro de modulos

--- Backend/project/apps/FinancialInformation/admin.py
+++ Backend/project/apps/FinancialInformation/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import WorkingInformation, OtherSourcesOfIncome, Reference
 # Register your models here.
+
+admin.site.register(WorkingInformation)
+admin.site.register(OtherSourcesOfIncome)
+admin.site.register(Reference)
