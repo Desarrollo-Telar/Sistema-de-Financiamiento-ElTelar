36dbdfae4a0b065c443503e532d78cadb2aaf602
choc_1403 <eloicx@gmail.com>
2024-05-23 12:36:37 -0600

    Formulario informacion personal

--- Backend/project/apps/customers/models.py
+++ Backend/project/apps/customers/models.py
@@ -55,21 +55,21 @@ class Customer(models.Model):
     user_id = models.ForeignKey(User, blank=False, null=False, on_delete=models.CASCADE)
     immigration_status_id = models.ForeignKey(ImmigrationStatus, blank=False, null=False, on_delete=models.CASCADE)
     customer_code = models.CharField(max_length=25, blank=False, null=False, unique=True)
-    first_name = models.TextField(max_length=100, blank=False, null=False)
-    last_name = models.TextField(max_length=100, blank=False, null=False)
-    type_identification = models.CharField(choices=identification, default='DPI', max_length=50 )
-    identification_number = models.CharField(max_length=15, blank=False, null=False, unique=True)
-    telephone = models.CharField(max_length=20, blank=True, null=True)
-    email = models.EmailField(unique=True)
+    first_name = models.TextField(max_length=100, blank=False, null=False) #
+    last_name = models.TextField(max_length=100, blank=False, null=False) #
+    type_identification = models.CharField(choices=identification, default='DPI', max_length=50 ) #
+    identification_number = models.CharField(max_length=15, blank=False, null=False, unique=True) #
+    telephone = models.CharField(max_length=20, blank=True, null=True) #
+    email = models.EmailField(unique=True) #
     status = models.CharField(choices=status, default='Posible Cliente', max_length=75)
-    date_birth = models.DateField()
+    date_birth = models.DateField() #
     number_nit = models.CharField(max_length=20, blank=False, null=False, unique=True) # NIT
-    place_birth = models.CharField(max_length=75, blank=False, null=False) # Lugar de nacimiento
-    marital_status =  models.CharField(max_length=50, blank=False, null=False)   # Estado civil
-    profession_trade = models.CharField(max_length=75, blank=False, null=False) # Profesion u oficio
-    gender = models.CharField(choices=genders, default='MASCULINO', max_length=50)
-    nationality = models.CharField(max_length=75, blank=False, null=False, default='Guatemala')
-    person_type = models.CharField(choices=type_person, max_length=50)
+    place_birth = models.CharField(max_length=75, blank=False, null=False) # Lugar de nacimiento #
+    marital_status =  models.CharField(max_length=50, blank=False, null=False)   # Estado civil #
+    profession_trade = models.CharField(max_length=75, blank=False, null=False) # Profesion u oficio #
+    gender = models.CharField(choices=genders, default='MASCULINO', max_length=50) #
+    nationality = models.CharField(max_length=75, blank=False, null=False, default='Guatemala') #
+    person_type = models.CharField(choices=type_person, max_length=50) #
     creation_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
