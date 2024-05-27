18e938e177b6199e637c6b467b5c21a770d4e76d
choc_1403 <eloicx@gmail.com>
2024-05-27 14:42:40 -0600

    Organizador de urls de apis

---
+++ Backend/project/static/js/API/urls_api.js
@@ -0,0 +1,9 @@
+export const urls = {
+    api_url_cliente : 'http://127.0.0.1:8000/customers/api/customers/',
+    api_url_condicion_migratoria : 'http://127.0.0.1:8000/customers/api/immigration_status/',
+    api_url_direccion: 'http://127.0.0.1:8000/addresses/api/address/',
+    api_url_referencia: 'http://127.0.0.1:8000/financial_information/api/reference/',
+    api_url_informacion_laboral:'http://127.0.0.1:8000/financial_information/api/working_information/',
+    api_url_otra_informacion_laboral:'http://127.0.0.1:8000/financial_information/api/other_sources/',
+    api_url_usuarios:'http://127.0.0.1:8000/users/api/users/',
+}
