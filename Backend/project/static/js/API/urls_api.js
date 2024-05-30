94634ea4ae4235c2fee314f4f06b5c7ff3501d3d
choc_1403 <eloicx@gmail.com>
2024-05-29 19:24:37 -0600

    Agregando las URLs de las API creadas

--- Backend/project/static/js/API/urls_api.js
+++ Backend/project/static/js/API/urls_api.js
@@ -8,4 +8,7 @@ export const urls = {
     api_url_usuarios:'http://127.0.0.1:8000/users/api/users/',
     api_url_rol:'http://127.0.0.1:8000/roles&permissions/api/role/',
     api_url_investment_plan:'http://127.0.0.1:8000/plan_inversion/api/plan_inversion/',
+    api_url_imagen:'http://127.0.0.1:8000/imagen/api/imagen/',
+    api_url_imagen_cliente:'http://127.0.0.1:8000/imagen/api/imagen_cliente/',
+    api_url_imagen_direccion:'http://127.0.0.1:8000/imagen/api/imagen_direccion/',
 }
