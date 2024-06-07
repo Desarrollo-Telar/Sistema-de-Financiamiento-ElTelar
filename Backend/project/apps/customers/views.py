230ac3d3d539dc835bc9e2daff131e4b07774593
choc_1403 <eloicx@gmail.com>
2024-06-07 11:49:09 -0600

    Clasificacion de informacion de un cliente

--- Backend/project/apps/customers/views.py
+++ Backend/project/apps/customers/views.py
@@ -111,12 +111,11 @@ class CustomerSearch(ListView):
 @usuario_activo
 def detail_customer(request,customer_code):
     template_name = 'customer/detail.html'
-    customer_list = get_object_or_404(Customer,customer_code = str(customer_code))
-    customer_id = customer_list.id
-    informacion_laboral = WorkingInformation.objects.filter(Q(customer_id=customer_id))
-    otra_informacion_laboral = OtherSourcesOfIncome.objects.filter(Q(customer_id=customer_id))
-    direccion = Address.objects.filter(Q(customer_id=customer_id))
-    plan_inversion = InvestmentPlan.objects.filter(Q(customer_id=customer_id))
+    customer_list = get_object_or_404(Customer,customer_code = str(customer_code))    
+    informacion_laboral = WorkingInformation.objects.filter(Q(customer_id=customer_list))
+    otra_informacion_laboral = OtherSourcesOfIncome.objects.filter(Q(customer_id=customer_list))
+    direccion = Address.objects.filter(Q(customer_id=customer_list))
+    plan_inversion = InvestmentPlan.objects.filter(Q(customer_id=customer_list))
 
     
     
