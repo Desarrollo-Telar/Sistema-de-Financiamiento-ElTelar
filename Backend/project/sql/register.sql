7631bb1278f418b090a00e8c964633995df384d9
choc_1403 <eloicx@gmail.com>
2024-06-15 07:01:00 -0600

    Mostrando informacion

--- Backend/project/sql/register.sql
+++ Backend/project/sql/register.sql
@@ -22,6 +22,30 @@ VALUES ('2024-N1', 'Jane', 'Smith', 'DPI', '987654321', '555-987', 'janesmith@ex
 INSERT INTO customers_customer(customer_code,first_name,last_name,type_identification,identification_number,telephone,email,status,date_birth,number_nit,place_birth,marital_status,profession_trade,gender,nationality,person_type,user_id_id,immigration_status_id_id,creation_date)
 VALUES ('2024-1', 'Alice', 'Johnson', 'DPI', '456789123', '555-456', 'alicejohnson@example.com', 'Aprobado', '1992-03-20', '459123-2', 'Chicago', 'married', 'Artist', 'FEMENINO', 'American', 'Indivicual (PI)', 1, 1,'2024-06-04 12:30:00');
 
+
+INSERT INTO FinancialInformation_reference(full_name, phone_number,reference_type, customer_id_id) 
+VALUES('JUAN CARLOS CHOC XOL','42249955','Personales',1),
+('Héctor Medina Castro','42249955','Personales',1),
+('Santiago Vázquez León','42249955','Bancarias',1),
+('Renata Campos Estrada','42249955','Bancarias',1);
+
+
+INSERT INTO FinancialInformation_othersourcesofincome(source_of_income,nit,phone_number,salary,customer_id_id)
+VALUES('DSAD','1808964-K','42249955','35',1);
+
+
+
+INSERT INTO addresses_address(street, number, city, state, postal_code, country, type_address, customer_id_id)
+VALUES
+('Calle Falsa 123', 456, 'Springfield', 'IL', '62704', 'USA', 'Dirección de Casa', 1),
+('Avenida Siempre Viva 742', 100, 'Springfield', 'IL', '62705', 'USA', 'Dirección de Trabajo', 1);
+
+INSERT INTO addresses_coordinate(latitud, longitud, address_id_id)
+VALUES
+(39.7817, -89.6501, 1),
+(39.7817, -89.6502, 2);
+
+
 -- MOSTRAR LA INFORMACION DE LOS CLIENTES
 SELECT * FROM customers_customer\G;
 
