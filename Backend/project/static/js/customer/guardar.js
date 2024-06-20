db29f8cf121e9e830f181bd17323877ae49cabea
choc_1403 <eloicx@gmail.com>
2024-06-20 15:57:42 -0600

    Validaciones a todo el formulario

--- Backend/project/static/js/customer/guardar.js
+++ Backend/project/static/js/customer/guardar.js
@@ -5,64 +5,77 @@ import { postPlanInversion } from '../API/investmentplan/post_api.js';
 import { postLaboral } from '../API/workinginformation/post_api.js';
 import { postReferencia } from '../API/reference/post_api.js';
 
-
-import {recoletarInformacionCliente, recoletarInformacionDirecciones,recolectarInformacionLaboral,recoletarInformacionPlanInversion,recoletarInformacionReferencias} from '../customer/recolectar.js'
+import {
+    recoletarInformacionCliente, 
+    recoletarInformacionDirecciones,
+    recolectarInformacionLaboral,
+    recoletarInformacionPlanInversion,
+    recoletarInformacionReferencias
+} from '../customer/recolectar.js';
 
 document.getElementById('customer').addEventListener('submit', async function (event) {
     event.preventDefault();
-    let customer_id;
 
     try {
+        // Recolectar y validar información del cliente
+        const infoCliente = recoletarInformacionCliente();
+        if (!infoCliente.validar()) {
+            alert('No se puede proceder con el registro del cliente debido a información incompleta. Por favor, revise todos los campos requeridos.');
+            return;
+        }
         
+        const infoDireccion = recoletarInformacionDirecciones();
+        if (!infoDireccion.validar()) {
+            alert('No se pudo registrar la dirección del cliente. Por favor, asegúrese de proporcionar información completa sobre la dirección de trabajo o de residencia.');
+            return;
+        }
         
-        recoletarInformacionCliente();
+        const infoLaboral = recolectarInformacionLaboral();
+        if (!infoLaboral.validar()) {
+            alert('No se pudo registrar la información laboral del cliente. Por favor, complete todos los campos necesarios en la descripción de la información laboral.');
+            return;
+        }
+        
+        const infoPlanInversion = recoletarInformacionPlanInversion();
+        if (!infoPlanInversion.validar()) {
+            alert('No se pudo registrar el plan de inversión del cliente. Asegúrese de proporcionar toda la información requerida.');
+            return;
+        }
+        
+        const infoReferencias = recoletarInformacionReferencias();
+        if (!infoReferencias.validar()) {
+            alert('No se pudieron registrar las referencias del cliente debido a información incompleta. Por favor, revise los detalles de las referencias.');
+            return;
+        }
+        
+
+        // Realizar llamadas a la API
         const customerData = await postCustomer(urls.api_url_cliente);
         console.log('Cliente registrado con éxito:', customerData);
-        customer_id = customerData.id;
+        const customer_id = customerData.id;
 
-        // Registra la dirección
-    
-        recoletarInformacionDirecciones(customer_id);
         const direccionData = await postDireccion(urls.api_url_direccion, customer_id);
         console.log('Dirección registrada con éxito:', direccionData);
 
-        // Registra la información laboral
-        recolectarInformacionLaboral(customer_id);
         const laboralData = await postLaboral(customer_id);
         console.log('Información laboral registrada con éxito:', laboralData);
 
-        // Registra el plan de inversión
-        
-        recoletarInformacionPlanInversion(customer_id);
         const planInversionData = await postPlanInversion(urls.api_url_investment_plan, customer_id);
         console.log('Plan de inversión registrado con éxito:', planInversionData);
 
-        // Registra las referencias
-        recoletarInformacionReferencias(customer_id);
         const referenciaData = await postReferencia(urls.api_url_referencia, customer_id);
         console.log('Referencias guardadas con éxito:', referenciaData);
 
         alert('¡Formulario enviado con éxito!');
-        // Redirigir a la página de éxito después de enviar todas las referencias
-        let hostname = window.location.hostname;
-        let protocol = window.location.protocol;
-        let port = window.location.port;
-        let fullUrl = window.location.href;
-        //http://127.0.0.1:8000/qr/http://127.0.0.1:8000/formulario_ive/<int:id>//
-        //formulario_ive/<int:id>/
-        let generar = `${protocol}//${hostname}:${port}/qr/${protocol}//${hostname}:${port}/formulario_ive/${customer_id}//`;
 
+        // Redirigir a la página de éxito
+        const { protocol, hostname, port } = window.location;
+        const generar = `${protocol}//${hostname}:${port}/qr/${protocol}//${hostname}:${port}/formulario_ive/${customer_id}//`;
         console.log(generar);
-        //window.location.href = generar;
         window.location.href = '/customers/';
-        
         
     } catch (error) {
         console.error('Error al registrar los datos:', error);
-        //window.location.href = '/customers/delete/'+customer_id+'/';
         alert('Hubo un error al enviar el formulario. Por favor, inténtalo de nuevo.');
     }
-
-
 });
-
