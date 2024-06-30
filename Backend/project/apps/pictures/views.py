d96d77aa4d5535c26efe43712c0a8a7afe8b0706
choc1403 <eloicx@gmail.com>
2024-06-30 10:53:12 -0600

    Funciones para guardar imagenes

--- Backend/project/apps/pictures/views.py
+++ Backend/project/apps/pictures/views.py
@@ -1,3 +1,103 @@
-from django.shortcuts import render
+# URL
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
+
+# FORMS
+from .forms import ImagenForms
+
+# MODELS
+from .models import Imagen, ImagenAddress, ImagenCustomer, ImagenGuarantee, ImagenOther
+from apps.customers.models import Customer
+from apps.addresses.models import Address
+from apps.InvestmentPlan.models import InvestmentPlan
+
+# CRUD
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic import CreateView, DeleteView, UpdateView
+from django.db.models import Q 
+
+# DECORADORES
+from django.contrib.auth.decorators import login_required
+from project.decorador import usuario_activo
+from django.utils.decorators import method_decorator
 
 # Create your views here.
+
+@login_required
+@usuario_activo
+def create_imagen_customer(request, customer_code):
+    template_name = ''
+    customer_id = get_object_or_404(Customer, customer_code = customer_code)
+    if request.method == 'POST':
+        form = ImagenForms(request.POST)
+        if form.is_valid():
+            imagen = Imagen()
+            imagen.image = form.cleaned_data.get('image')
+            imagen.description = form.cleaned_data.get('description')
+            imagen.save()
+            imagen_customer = ImagenCustomer(customer_id=customer_id, image_id=imagen)
+            imagen_customer.save()
+            return redirect('customers:detail',customer_code)
+        
+    form = ImagenForms
+    context = {
+        'form': form
+    }
+
+    return render(request, template_name, context)
+
+@login_required
+@usuario_activo
+def create_imagen_customer_address(request,address_id_id, customer_code):
+    template_name = ''
+    customer_id = get_object_or_404(Customer, customer_code = customer_code)
+    address_id = get_object_or_404(Address, id=address_id_id)
+    if request.method == 'POST':
+        form = ImagenForms(request.POST)
+        if form.is_valid():
+            imagen = Imagen()
+            imagen.image = form.cleaned_data.get('image')
+            imagen.description = form.cleaned_data.get('description')
+            imagen.save()
+            
+            imagen_address = ImagenAddress(address_id=address_id, image_id=imagen, customer_id=customer_id)
+            imagen_address.save()
+
+
+            return redirect('customers:detail',customer_code)
+        
+    form = ImagenForms
+    context = {
+        'form': form
+    }
+
+    return render(request, template_name, context)
+
+@login_required
+@usuario_activo
+def create_imagen_customer_guarantee(request,investment_plan_id_id, customer_code):
+    template_name = ''
+    customer_id = get_object_or_404(Customer, customer_code = customer_code)
+    investment_plan_id = get_object_or_404(InvestmentPlan, id=investment_plan_id_id)
+    if request.method == 'POST':
+        form = ImagenForms(request.POST)
+        if form.is_valid():
+            imagen = Imagen()
+            imagen.image = form.cleaned_data.get('image')
+            imagen.description = form.cleaned_data.get('description')
+            imagen.save()
+            
+            imagen_address = ImagenGuarantee(investment_plan_id=investment_plan_id, image_id=imagen, customer_id=customer_id)
+            imagen_address.save()
+
+
+            return redirect('customers:detail',customer_code)
+        
+    form = ImagenForms
+    context = {
+        'form': form
+    }
+
+    return render(request, template_name, context)
