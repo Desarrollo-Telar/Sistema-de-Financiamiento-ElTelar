a6c9b9f634eed862c7cfdbe5c9fb12c449a05866
choc <eloicx@gmail.com>
2024-06-29 18:37:35 -0600

    Funcion para que funcione el formulario para registrar la informacion laboral de un cliente

--- Backend/project/apps/FinancialInformation/views.py
+++ Backend/project/apps/FinancialInformation/views.py
@@ -35,3 +35,33 @@ def delete_working_information(request,id, customer_code):
     working = get_object_or_404(WorkingInformation, id = id)
     working.delete()
     return redirect('customers:detail', customer_code)
+
+@login_required
+@usuario_activo
+def create_working_information(request, customer_code):
+    template_name = ''
+    customer_id = get_object_or_404(Customer, customer_code=str(customer_code))
+    
+    form = WorkingInformationForms
+    context = {
+        'form':form
+    }
+    if request.method == 'POST':
+        form = WorkingInformationForms(request.POST)
+        if form.is_valid():
+            working = WorkingInformation()
+            working.customer_id = customer_id
+            working.position = form.cleaned_data.get('position')
+            working.start_date = form.cleaned_data.get('start_date')
+            working.description = form.cleaned_data.get('description')
+            working.salary = form.cleaned_data.get('salary')
+            working.working_hours = form.cleaned_data.get('working_hours')
+            working.phone_number = form.cleaned_data.get('phone_number')
+            working.source_of_income = form.cleaned_data.get('source_of_income')
+            working.income_detail = form.cleaned_data.get('income_detail')
+            working.employment_status = form.cleaned_data.get('employment_status')
+            working.company_name = form.cleaned_data.get('company_name')
+            working.save()
+
+    return render(request, template_name, context)
+
