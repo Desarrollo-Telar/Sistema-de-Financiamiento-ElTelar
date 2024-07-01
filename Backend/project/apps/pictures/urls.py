79f0603ffd6d04ffa4ff9a3453e4a7973f0fb603
choc1403 <eloicx@gmail.com>
2024-07-01 14:58:13 -0600

    URL para el modulo de imagenes

--- Backend/project/apps/pictures/urls.py
+++ Backend/project/apps/pictures/urls.py
@@ -4,9 +4,21 @@ from django.urls import path, include
 # API
 from apps.pictures.api import routers
 
+# Decorador
+from django.contrib.auth.decorators import login_required
+
+# VIEWS
+from . import views
+
 app_name = 'imagen'
 
 urlpatterns = [
+    path('create_image/<str:customer_code>/', views.create_imagen_customer, name='create_image'),
+    path('create_image/address/<int:id>/<str:customer_code>/', views.create_imagen_customer_address, name='create_image_address'),
+    path('create_image/guarantee/<int:id>/<str:customer_code>/', views.create_imagen_customer_guarantee, name='create_image_guarantee'),
+    path('delete_image/<int:id>/<str:customer_code>/',views.delete_imagen, name='delete_image'),
+    path('update_image/<int:id>/<str:customer_code>/',views.update_imagen, name='update_image'),
+
     
 ]
 
