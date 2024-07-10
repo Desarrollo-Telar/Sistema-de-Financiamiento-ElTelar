247fb401a2243e21282399d7d6d3c51642683670
choc_1403 <eloicx@gmail.com>
2024-07-10 17:13:44 -0600

    Eliminar la tabla de coordenadas

--- Backend/project/apps/addresses/admin.py
+++ Backend/project/apps/addresses/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Address, Coordinate
+from .models import Address
 
 # Register your models here.
 @admin.register(Address)
@@ -13,20 +13,11 @@ class AddressAdmin(admin.ModelAdmin):
             
             'country',
             'type_address',
+            'latitud',
+            'longitud',
             'customer_id'
     )
     list_display = ('street','number','city','state','country')
     search_fields = ('street','number','city','state','country','type_address','customer_id')
     list_filter=('customer_id','type_address')
 
-@admin.register(Coordinate)
-class CoordinateAdmin(admin.ModelAdmin):
-    fields = (
-        'latitud',
-        'longitud',
-        'address_id'
-    )
-
-    list_display = ('address_id','latitud','longitud')
-    search_fields = ('address_id','latitud','longitud')
-    list_filter=('address_id',)
