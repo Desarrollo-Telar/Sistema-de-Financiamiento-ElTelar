7ee83a94e28020b5e7f40179b2f96c374305acb1
choc_1403 <eloicx@gmail.com>
2024-07-17 09:27:34 -0600

    Solucionar

--- Backend/project/apps/financings/clases/paymentplan.py
+++ Backend/project/apps/financings/clases/paymentplan.py
@@ -37,18 +37,24 @@ class PaymentPlan:
         return round(self.__credit_id.monto, 2)
 
     def calculo_intereses(self, monto=None):
+        interese = None
         if self.formaPago() == 'NIVELADA':
             intereses = (monto * self.interes()) / 12
-        intereses = (self.monto_inicial() * self.interes()) / 12
+        else:
+            intereses = (self.monto_inicial() * self.interes()) / 12
         return round(intereses, 2)
 
-    def calculo_cuota(self):
+    def calculo_cuota(self,interes=None, capital=None):
+        cuota = None
         if self.formaPago() == 'NIVELADA':
             default_interes = self.interes() / 12
             parte1 = (1 + default_interes) ** self.__plazo * default_interes
             parte2 = (1 + default_interes) ** self.__plazo - 1
             cuota = (parte1 / parte2) * self.monto_inicial()
-            return round(cuota, 2)
+            
+        else:
+            cuota = interes + capital
+        return round(cuota, 2)
 
     def calculo_capital(self, cuota=None, intereses=None):
         if self.formaPago() == 'NIVELADA':
@@ -57,54 +63,80 @@ class PaymentPlan:
             return round(self.monto_inicial()/self.__plazo,2)
 
     def inicial(self):
+        dicio = {}
+        dicio['mes']=1
+        dicio['monto_prestado']=round(self.monto_inicial(),2)
         if self.formaPago() == 'NIVELADA':
             intereses = self.calculo_intereses(self.monto_inicial())
             cuota = self.calculo_cuota()
             capital = self.calculo_capital(cuota, intereses)
-            dicio = {
-                'mes': 1,
-                'monto_prestado': round(self.monto_inicial(), 2),
-                'intereses': intereses,
-                'capital': capital,
-                'cuota': cuota,
-            }
-            return dicio
+            dicio['intereses']=intereses
+            dicio['capital']=capital
+            dicio['cuota']=cuota
+            
+            
+        else:
+            intereses = self.calculo_intereses(self.monto_inicial())
+            capital = self.calculo_capital()
+            cuota = self.calculo_cuota(intereses,capital)
+            dicio['intereses']=intereses
+            dicio['capital']=capital
+            dicio['cuota']=cuota
+        return dicio
+
 
     def generar_plan(self):
-        plan = []
+        plan = [self.inicial()]
+        
 
-        plan.append(self.inicial())
-
-        if self.formaPago() == 'NIVELADA':
+        
+        if self.formaPago() == 'NIVELADA': 
             for mes in range(2, self.plazo + 1):
+                dicio = {}
                 anterior = plan[mes - 2]
-
                 monto_prestado = round(anterior['monto_prestado'] - anterior['capital'], 2)
                 intereses = self.calculo_intereses(monto_prestado)
-                cuota = self.calculo_cuota()
-                capital = self.calculo_capital(cuota, intereses)
+                dicio['mes']=mes
+                dicio['monto_prestado']=monto_prestado
+                dicio['intereses'] = intereses
 
-                dicio = {
-                    'mes': mes,
-                    'monto_prestado': round(monto_prestado, 2),
-                    'intereses': round(intereses, 2),
-                    'capital': round(capital, 2),
-                    'cuota': cuota,  
-                }
+                cuota = self.calculo_cuota() 
+                capital = self.calculo_capital(cuota, intereses)     
+                      
+                dicio['capital'] = capital
+                dicio['cuota']=cuota
+
                 plan.append(dicio)
-            return plan
+        else:
+            for mes in range(2, self.plazo + 1):
+                dicio = {}
+                anterior = plan[mes - 2]
+                monto_prestado = round(anterior['monto_prestado'] - anterior['capital'], 2)
+                intereses = self.calculo_intereses(monto_prestado)
+                dicio['mes']=mes
+                dicio['monto_prestado']=monto_prestado
+                dicio['intereses'] = intereses
+                capital = round(anterior['capital'],2)
+                cuota = self.calculo_cuota(intereses,capital)
+                dicio['capital'] = capital
+                dicio['cuota']=cuota
+                plan.append(dicio)
+
+        return plan
+                
+       
+            
 
 
 if __name__ == '__main__':
     fiador = Customer('Juan', 'Lopez', 'lopez@gmail.com', 'DPI', '323846682', '1106369', '42256694', 'RESIDENTE', 'Aprobado', 'MASCULINO', 'AGRONOMO', 'GUATEMALTECA', 'COBAN', '14-03-1995', 'SOLTERO', 'Indivicual (PI)')
     cliente = Customer('Juan', 'Lopez', 'lopez@gmail.com', 'DPI', '323846682', '1106369', '42256694', 'RESIDENTE', 'Aprobado', 'MASCULINO', 'AGRONOMO', 'GUATEMALTECA', 'COBAN', '14-03-1995', 'SOLTERO', 'Indivicual (PI)')
     destino = InvestmentPlan('CONSUMO', 1500, 750, 100, cliente)
-    credito = Credit(destino.type_of_product_or_service, 117000, 60, 66, 'AMORTIZACIONES A CAPITAL', 'MENSUAL', '2024-07-14', 'CONSUMO', destino, fiador)
+    credito = Credit(destino.type_of_product_or_service, 117000, 60, 66, 'NIVELADA', 'MENSUAL', '2024-07-14', 'CONSUMO', destino, fiador)
     plan_pago = PaymentPlan(credito)
 
-    print(plan_pago.calculo_intereses())
-"""
+    print(plan_pago.inicial())
+
     plan = plan_pago.generar_plan()
     for pago in plan:
         print(pago)
-"""
