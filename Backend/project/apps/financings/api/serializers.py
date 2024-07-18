47c890fd43a22e3730d4d75f04b775d6a9f88f36
choc_1403 <eloicx@gmail.com>
2024-07-18 14:53:08 -0600

    API

--- Backend/project/apps/financings/api/serializers.py
+++ Backend/project/apps/financings/api/serializers.py
@@ -2,12 +2,31 @@
 from rest_framework import serializers
 
 # MODELS
-from apps.financings.models import Credit
+from apps.financings.models import Credit, Guarantees, DetailsGuarantees
 
 class CreditSerializer(serializers.ModelSerializer):
     class Meta:
         model = Credit
         fields = [
             'proposito',
-            ''
+            'monto',
+            'plazo',
+            'tasa_interes',
+            'forma_de_pago',
+            'frecuencia_pago',
+            'fecha_inicio',
+            'fecha_vencimiento',
+            'tipo_credito',
+            'destino_id',
+            'customer_id'
         ]
+
+class GuaranteesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Guarantees
+        fields = '__all__'
+
+class DetailsGuaranteesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DetailsGuarantees
+        fields = '__all__'
--- Backend/project/apps/financings/api/views.py
+++ Backend/project/apps/financings/api/views.py
@@ -1,10 +1,22 @@
 # Serializador
-from .serializers import CreditSerializer
+from .serializers import CreditSerializer, GuaranteesSerializer, DetailsGuaranteesSerializer
 
 # MODELS
-from apps.financings.models import Credit
+from apps.financings.models import Credit, Guarantees, DetailsGuarantees
 
 # API
 from rest_framework import viewsets, status, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+class CreditViewSet(viewsets.ModelViewSet):
+    serializer_class = CreditSerializer
+    queryset = Credit.objects.all()
+
+class GuaranteesViewSet(viewsets.ModelViewSet):
+    serializer_class = GuaranteesSerializer
+    queryset = Guarantees.objects.all()
+
+class DetailsGuaranteesViewSet(viewsets.ModelViewSet):
+    serializer_class = DetailsGuaranteesSerializer
+    queryset = DetailsGuarantees.objects.all()
