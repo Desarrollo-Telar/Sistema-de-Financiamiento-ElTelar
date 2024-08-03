09f0ee1a2bd06554e3e2801f2e157ac4f4b46d80
choc_1403 <eloicx@gmail.com>
2024-08-03 04:35:54 -0600

    Control de desembolsos

--- Backend/project/static/js/financings/credit/disbursement.js
+++ Backend/project/static/js/financings/credit/disbursement.js
@@ -3,21 +3,42 @@ import { Desembolso } from '../../class/disbursement.js';
 export const desembolso = new Desembolso();
 
 // Función para actualizar el total a depositar
-function actualizarTotalDepositar() {
+var suma = 0;
+async function actualizarTotalDepositar() {
     const monto = parseFloat(document.getElementById('monto').value) || 0;
     const poliza_seguro = parseFloat(document.getElementById('poliza_seguro').value) || 0;
     const honorarios = parseFloat(document.getElementById('honorarios').value) || 0;
     const saldo_anterior = parseFloat(document.getElementById('saldo_anterior').value) || 0;
+    const credi_id = parseInt(document.getElementById('credit_id').value) || 0;
 
     // Actualiza los valores en la instancia de Desembolso
     desembolso.monto_credito = monto;
     desembolso.poliza_seguro = poliza_seguro;
     desembolso.honorarios = honorarios;
-    desembolso.saldo_anterior = saldo_anterior; // Ajusta según sea necesario
+    desembolso.saldo_anterior = saldo_anterior; 
 
-    // Usa la propiedad total_a_depositar de Desembolso
+    suma = 0;
     const total = desembolso.total_a_depositar;
-    
+
+    try {
+        const laboral = await filtro(credi_id);
+
+        if (Array.isArray(laboral)) {
+            laboral.forEach(element => {
+                suma += parseFloat(element['monto_total_desembolso']);
+            });
+        }
+
+        if (suma >= total) {
+            alert('NO SE PUEDE REALIZAR OTRO DESEMBOLSO');
+            window.location.href = '/financings/disbursement/';
+        }
+
+        console.log('Suma total de desembolsos:', suma);
+    } catch (error) {
+        console.error('Error obteniendo detalles del cliente:', error);
+    }
+
     // Actualiza el valor en el elemento HTML
     document.getElementById('total_depositar').value = Math.round(total);
 }
@@ -30,26 +51,21 @@ document.getElementById('honorarios').addEventListener('input', actualizarTotalD
 document.getElementById('desembolso').addEventListener('submit', async function (event) {
     event.preventDefault();
     try {
-        const credi_id = document.getElementById('credit_id').value;
+        const credi_id = parseInt(document.getElementById('credit_id').value);
 
-       
-        const desembolsos = await registrarDesembolso('http://127.0.0.1:8000/financings/api/desembolso/',credi_id);
-        console.log(desembolsos)
+        const desembolsos = await registrarDesembolso('http://127.0.0.1:8000/financings/api/desembolso/', credi_id);
+        console.log(desembolsos);
         alert('¡Formulario enviado con éxito!');
         window.location.href = '/financings/disbursement/';
-
-
     } catch (error) {
         console.error('Error al registrar los datos:', error);
         alert('Hubo un error al enviar el formulario. Por favor, inténtalo de nuevo.');
     }
 });
 
-async function registrarDesembolso(url, credit_id){
+async function registrarDesembolso(url, credit_id) {
     try {
-        desembolso.credit_id = credit_id
-
-        
+        desembolso.credit_id = credit_id;
 
         const csrfTokenElement = document.querySelector('meta[name="csrf-token"]');
         if (!csrfTokenElement) {
@@ -72,11 +88,45 @@ async function registrarDesembolso(url, credit_id){
 
         const data = await response.json();
         console.log(data);
-        
+
         return data;
     } catch (error) {
         console.error('Error:', error);
         throw error;
     }
+}
 
+async function informacionDesembolso() {
+    return fetch('http://127.0.0.1:8000/financings/api/desembolso/')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al obtener información de desembolso: ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Lista de desembolsos obtenida:', data);
+            return data;
+        })
+        .catch(error => {
+            console.error('Error al obtener la lista de desembolsos:', error);
+            throw error;
+        });
+}
+
+async function filtro(valor) {
+    try {
+        const desembolso = await informacionDesembolso();
+        let filterList = [];
+
+        if (desembolso && Array.isArray(desembolso) && desembolso.length > 0) {
+            filterList = desembolso.filter(item => item['credit_id'] === valor);
+        }
+
+        console.log(filterList);
+        return filterList;
+    } catch (error) {
+        console.error('Error en el filtro', error);
+        throw error;
+    }
 }
