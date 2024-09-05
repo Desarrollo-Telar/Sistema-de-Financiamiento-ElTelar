01aba97aa59fd9c5d21aff761d16b8b0616016fc
choc_1403 <eloicx@gmail.com>
2024-09-05 13:44:00 -0600

    Intento de subir documento

--- Backend/project/apps/documents/models.py
+++ Backend/project/apps/documents/models.py
@@ -90,7 +90,44 @@ def delete_document_files(sender, instance, **kwargs):
     if instance.document:
         instance.document.delete()
 
-@receiver(pre_save, sender=DocumentBank)
+import csv
+import os
+import pandas as pd
+@receiver(post_save, sender=DocumentBank)
 def registrar_pagos(sender, instance, **kwargs):
-    file_path = instance.document
-    print(file_path)
+    file_path = f'media/{instance.document}'
+    nuevo = 'apps/financings/clases/buenoo.csv'
+
+    # Elimina el archivo si ya existe antes de empezar a escribir
+    if os.path.exists(nuevo):
+        os.remove(nuevo)
+
+
+    # Función para crear un archivo nuevo y escribir en él
+    def crear_archivo_nuevo(info):
+        with open(nuevo, 'a', newline='') as archivo:
+            writer = csv.writer(archivo)
+            writer.writerow(info)
+
+    # Lee el archivo CSV original y escribe el nuevo archivo filtrado
+    with open(file_path, newline='',encoding='ISO-8859-1') as csvfile:
+        file = csv.reader(csvfile, delimiter=',')
+
+        # Variable para activar la captura de los movimientos cuando se encuentra el encabezado
+        capture_data = False
+
+        for row in file:
+            # Detecta el encabezado para comenzar a capturar los datos relevantes
+            if row == ['Fecha', 'Oficina', 'Descripciï¿½n', 'Referencia', 'Secuencial', 'Cheque Propio / Local / Efectivo', 'Dï¿½bito (-)', 'Crï¿½dito (+)', 'Saldo Contable', 'Saldo Disponible']:
+                capture_data = True
+                print(row)
+                crear_archivo_nuevo(row)  # Escribe el encabezado
+                continue
+
+            # Si ya estamos capturando datos, guarda las filas no vacías que siguen al encabezado
+            if capture_data and row:
+                if row != ['Confidencial']:  # Evita filas con "Confidencial"
+                    crear_archivo_nuevo(row)
+                    print(row)
+
+    
