a8d502fdf9944efc4deee655751c637833193593
choc_1403 <eloicx@gmail.com>
2024-09-27 15:37:55 -0600

    Quitar elementos

--- Backend/project/staticfiles/js/API/address/list_api.js
+++
@@ -1,7 +0,0 @@
-
-import { urls } from '../urls_api.js'
-
-export const list_customer = fetch(urls.api_url_direccion)
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error('Error:', error));
