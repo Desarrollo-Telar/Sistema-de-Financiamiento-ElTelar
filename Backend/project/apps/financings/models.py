850f930652cd9b760ceff3be35d17f4ef1a3365d
choc_1403 <eloicx@gmail.com>
2024-10-03 13:16:16 -0600

    Arreglo en el atributo de tasa de interes

--- Backend/project/apps/financings/models.py
+++ Backend/project/apps/financings/models.py
@@ -46,7 +46,7 @@ class Credit(models.Model):
     proposito = models.TextField("Proposito", blank=False, null=False)
     monto = models.DecimalField("Monto", decimal_places=2, max_digits=15, blank=False, null=False)
     plazo = models.IntegerField("Plazo", blank=False, null=False)
-    tasa_interes = models.DecimalField("Tasa de Interes", max_digits=5, decimal_places=2, null=False, blank=False)
+    tasa_interes = models.DecimalField("Tasa de Interes", max_digits=5, decimal_places=3, null=False, blank=False)
     forma_de_pago = models.CharField("Forma de Pago", choices=formaPago, max_length=75, blank=False, null=False, default='NIVELADA')
     frecuencia_pago = models.CharField("Frecuencia de Pago", choices=frecuenciaPago, max_length=75, blank=False, null=False, default='MENSUAL')
     fecha_inicio = models.DateField("Fecha de Inicio", blank=False, null=False)
@@ -528,7 +528,10 @@ class PaymentPlan(models.Model):
         return round(self.mora_acumulada,2)
     
     def total(self):
-        total = self.interest+self.mora +self.calculo_capital()
+        if self.calculo_capital() > 0:
+            total = self.interest+self.mora +self.calculo_capital()
+        else:
+            total = self.interest+self.mora
         return round(total,2)
     
     def calculo_cuota(self):
