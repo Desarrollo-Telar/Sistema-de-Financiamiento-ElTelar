c8358d28502f6f951b6b1ce9096ac104881a4ef2
choc_1403 <eloicx@gmail.com>
2024-10-11 16:05:18 -0600

    Limitar solo el registro de un desembolso de Aplicacion de gastos por credito

--- Backend/project/apps/financings/signals/desembolso.py
+++ Backend/project/apps/financings/signals/desembolso.py
@@ -6,29 +6,39 @@ from apps.financings.models import AccountStatement, Disbursement
 
 
 import uuid
+from django.db import transaction, IntegrityError
 
 
 # EL DESEMBOLSO REALIZADO SE REFLEJA EN EL ESTADO DE CUENTAS DEL CLIENTE
 @receiver(post_save, sender=Disbursement)
 def reflejar_estado_cuenta(sender, instance, created, **kwargs):
     if created:
-        referencia = str(uuid.uuid4())[:8]
-        
-        # Si `numero_referencia` es único, manejamos colisión con try/except
-        estado_cuenta = AccountStatement(
-            credit=instance.credit_id,
-            disbursement=instance,
-            disbursement_paid=instance.monto_total_desembolso,
-            numero_referencia=referencia,
-            description=f'{instance.forma_desembolso}'
+        desembolso_credito = Disbursement.objects.filter(
+            credit_id_id=instance.credit_id.id, 
+            forma_desembolso='APLICACIÓN GASTOS'
         )
-        
-        try:
-            estado_cuenta.save()
-        except IntegrityError:
-            # En caso de colisión, generar una nueva referencia y volver a intentar
+
+        if desembolso_credito.count() > 1:
+            instance.delete()
+        else:
             referencia = str(uuid.uuid4())[:8]
-            estado_cuenta.numero_referencia = referencia
-            estado_cuenta.save()
+            
+            # Usar una transacción atómica para asegurar la consistencia
+            with transaction.atomic():
+                estado_cuenta = AccountStatement(
+                    credit=instance.credit_id,
+                    disbursement=instance,
+                    disbursement_paid=instance.monto_total_desembolso,
+                    numero_referencia=referencia,
+                    description=f'{instance.forma_desembolso}'
+                )
+
+                try:
+                    estado_cuenta.save()
+                except IntegrityError:
+                    # En caso de colisión, generar una nueva referencia y volver a intentar
+                    referencia = str(uuid.uuid4())[:8]
+                    estado_cuenta.numero_referencia = referencia
+                    estado_cuenta.save()
 
 
