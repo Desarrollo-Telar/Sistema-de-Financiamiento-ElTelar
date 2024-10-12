1f102dc6257d68f940266175ea59a529a8e30b51
choc_1403 <eloicx@gmail.com>
2024-10-12 14:55:13 -0600

    wsqi

--- Backend/project/project/settings.py
+++ Backend/project/project/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
-    'anymail',
+    #'anymail',
     'apps.users',
     'apps.customers',
     'apps.roles',
