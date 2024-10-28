4dc9a9c5bc42bfa3d520aa4803f3c1072719f2b5
choc_1403 <eloicx@gmail.com>
2024-10-28 15:49:42 -0600

    V002

--- Backend/project/apps/financings/signals/desembolso.py
+++ Backend/project/apps/financings/signals/desembolso.py
@@ -2,8 +2,10 @@ from django.db.models.signals import post_save, pre_save, pre_delete, post_delet
 from django.dispatch import receiver
 
 # MODELOS
-from apps.financings.models import AccountStatement, Disbursement, Credit
+from apps.financings.models import AccountStatement, Disbursement, Credit, PaymentPlan
 
+# CALCULOS
+from apps.financings.calculos import calculo_interes
 
 import uuid
 from django.db import transaction, IntegrityError
@@ -12,12 +14,31 @@ from django.db import transaction, IntegrityError
 # EL DESEMBOLSO REALIZADO SE REFLEJA EN EL ESTADO DE CUENTAS DEL CLIENTE
 @receiver(post_save, sender=Disbursement)
 def reflejar_estado_cuenta(sender, instance, created, **kwargs):
-    monto = instance.monto_credito
-    credit = Credit.objects.filter(id=instance.credit_id.id)
-    credit.monto = monto
-    credit.save()
-
     if created:
+        monto_agregado = instance.monto_credito
+        credit = Credit.objects.filter(id=instance.credit_id.id)
+       
+        monto_anterior = credit.monto
+        credit.monto = monto_agregado
+        diferencia_monto = monto_agregado - monto_anterior
+
+        credit.save()
+        cuota_actual = PaymentPlan.objects.filter(credit_id=credit).order_by('-id').first()
+        saldo_pendiente = cuota_actual.saldo_pendiente
+        interes_actual = cuota_actual.interest
+
+        interes = calculo_interes(saldo_pendiente, credit.tasa_interes)
+
+        diferencia_interes = interes_actual - interes
+        if diferencia_interes <=0:
+            nuevo_interes = calculo_interes(saldo_pendiente, credit.tasa_interes)
+            
+        nuevo_interes = nuevo_interes - interes_actual
+
+        cuota_actual.saldo_pendiente += diferencia_monto
+        cuota_actual.cambios = True
+        cuota_actual.save()
+
         desembolso_credito = Disbursement.objects.filter(
             credit_id_id=instance.credit_id.id, 
             forma_desembolso='APLICACIÓN GASTOS'
