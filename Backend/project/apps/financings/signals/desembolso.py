160563dc5228ee903ad487aa38ed6cfa051fe4b4
choc_1403 <eloicx@gmail.com>
2024-10-29 08:16:56 -0600

    Falta por estrucutrar bien receiver de desembolso

--- Backend/project/apps/financings/signals/desembolso.py
+++ Backend/project/apps/financings/signals/desembolso.py
@@ -2,8 +2,10 @@ from django.db.models.signals import post_save, pre_save, pre_delete, post_delet
 from django.dispatch import receiver
 
 # MODELOS
-from apps.financings.models import AccountStatement, Disbursement
+from apps.financings.models import AccountStatement, Disbursement, Credit, PaymentPlan
 
+# CALCULOS
+from apps.financings.calculos import calculo_interes
 
 import uuid
 from django.db import transaction, IntegrityError
@@ -13,6 +15,30 @@ from django.db import transaction, IntegrityError
 @receiver(post_save, sender=Disbursement)
 def reflejar_estado_cuenta(sender, instance, created, **kwargs):
     if created:
+        monto_agregado = instance.monto_credito
+        credit = Credit.objects.filter(id=instance.credit_id.id)
+       
+        monto_anterior = credit.monto
+        credit.monto = monto_agregado
+        diferencia_monto = monto_agregado - monto_anterior
+
+        credit.save()
+        cuota_actual = PaymentPlan.objects.filter(credit_id=credit).order_by('-id').first()
+        saldo_pendiente = cuota_actual.saldo_pendiente
+        interes_actual = cuota_actual.interest
+
+        interes = calculo_interes(saldo_pendiente, credit.tasa_interes)
+
+        diferencia_interes = interes_actual - interes
+        if diferencia_interes <=0:
+            nuevo_interes = calculo_interes(saldo_pendiente, credit.tasa_interes)
+            
+        nuevo_interes = nuevo_interes - interes_actual
+
+        cuota_actual.saldo_pendiente += diferencia_monto
+        cuota_actual.cambios = True
+        cuota_actual.save()
+
         desembolso_credito = Disbursement.objects.filter(
             credit_id_id=instance.credit_id.id, 
             forma_desembolso='APLICACIÓN GASTOS'
@@ -40,5 +66,4 @@ def reflejar_estado_cuenta(sender, instance, created, **kwargs):
                     referencia = str(uuid.uuid4())[:8]
                     estado_cuenta.numero_referencia = referencia
                     estado_cuenta.save()
-
 
