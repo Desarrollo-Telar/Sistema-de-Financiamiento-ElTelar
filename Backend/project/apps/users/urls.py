e1ff94a5ad76223fa375f98e3911395cc4a7603e
choc_1403 <eloicx@gmail.com>
2024-10-29 08:27:11 -0600

    Arreglos

--- Backend/project/apps/users/urls.py
+++ Backend/project/apps/users/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     path('create_user/', login_required(views.userCreateView.as_view()), name='create'),
     path('update_user/<int:pk>/', login_required(views.userUpdateView.as_view()), name='update'),
     path('deactivate_user/<int:id>/', login_required(views.deactivate), name='deactivate'),
+    path('active/<int:id>/', login_required(views.habilitar_usuario), name='activar'),
     path('detail_user/<str:username>/', views.detail_user, name='detail'),
     path('change_password/', login_required(views.ChangePassword.as_view()), name='update_password'),
     path('change_password_user/<int:id>/', login_required(views.change_password_user), name='update_password_user'),
