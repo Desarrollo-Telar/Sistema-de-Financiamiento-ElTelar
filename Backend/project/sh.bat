1e6c5192328fa3030e2bf0026d880c338906a88e
choc_1403 <eloicx@gmail.com>
2024-12-23 15:28:47 -0600

    Cambios desde 0

--- Backend/project/sh.bat
+++ Backend/project/sh.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @echo off
 setlocal enabledelayedexpansion
 
@@ -20,27 +19,3 @@ if %errorlevel% equ 0 (
 )
 
 endlocal
-=======
-@echo off
-setlocal enabledelayedexpansion
-
-:: Cambiar a tu directorio del repositorio local
-cd C:\ruta\a\tu\repositorio
-
-:: Verificar si hay cambios en el repositorio remoto
-git fetch
-
-:: Verificar si hay cambios nuevos para descargar
-git status | findstr /C:"Your branch is behind"
-
-:: Si hay cambios, ejecutar git pull
-if %errorlevel% equ 0 (
-    echo Hay nuevos cambios. Ejecutando git pull...
-    git pull
-) else (
-    echo No hay cambios nuevos.
-)
-
-endlocal
->>>>>>> server
-pause
