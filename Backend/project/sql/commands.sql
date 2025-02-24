2d31b79c1995cdb2aa56d3ec290d48c713331cfb
develtelar <develtelar@gmail.com>
2025-02-24 17:24:32 -0600

    Actualizaciones

--- Backend/project/sql/commands.sql
+++ Backend/project/sql/commands.sql
@@ -17,9 +17,9 @@ INNER JOIN table2
 ON table1.column = table2.column;
 
 -- UPDATE
-UPDATE table_name
-SET column1 = value1, column2 = value2, ...
-WHERE condition;
+UPDATE financings_payment
+SET tipo_pago = "CLIENTE" 
+WHERE id = 97;
 
 -- DELETE
 DELETE FROM table_name WHERE condition;
