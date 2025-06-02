40ff9e1c73eabce61d114bad729dffd43e52e9c6
choc <eloicx@gmail.com>
2025-06-02 17:12:08 -0600

    responsivo, algunas partes

--- Backend/project/project/pagination.py
+++ Backend/project/project/pagination.py
@@ -3,7 +3,7 @@ from django.core.paginator import Paginator
 
 # PAGINACION
 def paginacion(request,list):
-    paginator = Paginator(list, 250)
+    paginator = Paginator(list, 50)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return page_obj
