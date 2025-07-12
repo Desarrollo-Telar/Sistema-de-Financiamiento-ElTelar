fe74975ed21aacce92b8b1f191bde100a3e3240f
choc1403 <eloicx@gmail.com>
2025-07-12 16:04:16 -0600

    ajustes

--- Backend/project/apps/users/api/routers.py
+++ Backend/project/apps/users/api/routers.py
@@ -10,6 +10,7 @@ from . import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet, 'users')
 router.register(r'permisos_usuarios', views.PermisoUsuarioViewSet, 'permisos_usuarios')
+router.register(r'profile', views.ProfileViewSet, 'profile')
 
 urlpatterns = [
     path('api/', include(router.urls)),
--- Backend/project/apps/users/api/views.py
+++ Backend/project/apps/users/api/views.py
@@ -11,7 +11,34 @@ from rest_framework.response import Response
 
 
 
+class ProfileViewSet(viewsets.ViewSet):
+    def list(self, request):
+        user = request.user
 
+        return Response({
+            "id":user.id,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "username": user.username,
+            "email": user.email,
+            "type_identification": user.type_identification,
+            "identification_number": user.identification_number,
+            "telephone": user.telephone,
+            "gender": user.gender,
+            "nationality": user.nationality,
+            "profile_pic": user.profile_pic.url if user.profile_pic else None,
+            "status": user.status,
+            "rol": {
+                "id": user.rol.id,
+                "role_name": user.rol.role_name
+            } if user.rol else None,
+            "nit": user.nit,
+            "sucursal": {
+                "id": user.sucursal.id,
+                "nombre": user.sucursal.nombre
+            } if user.sucursal else None
+        })
+  
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
