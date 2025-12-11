1b216d6ecc59b401645fe0528fff4b754b55b7de
choc1403 <eloicx@gmail.com>
2025-12-11 14:04:01 -0600

    Arreglo de la gestion de cobranza

--- Backend/project/project/func/otro_test.py
+++ Backend/project/project/func/otro_test.py
@@ -21,6 +21,8 @@ from datetime import date
 from scripts.conversion_datos import model_to_dict, cambios_realizados
 from django.apps import apps
 from apps.subsidiaries.models import Subsidiary
+import uuid
+from django.db.models import Q
 
 def pruebas(dia, sucursal):
    from django.db import connection
@@ -30,28 +32,29 @@ def pruebas(dia, sucursal):
 
 
 if __name__ == '__main__':
-   cliente = Credit.objects.get(id = 513)
-   dia = '2025-12-03'
-   sucursal = Subsidiary.objects.get(id=1)
+   # Obtener el mes
+   dia = datetime.now().date()
+   mes = dia.month
+   nombre_mes = dia.strftime("%B")
 
-   print(model_to_dict(cliente))
+   print(f"El nombre del mes actual es: {nombre_mes}")
+
+   # Obtener todas las cobranzas con estado diferente de pendiente
+   filtros = Q()
+   filtros &= Q(estado_cobranza__icontains = 'INCUMPLIDO')
+   filtros &= Q(fecha_registro__month = mes)
    
+   cobranzas = Cobranza.objects.filter(filtros)
+
+   for cobranza in cobranzas:
+      fecha_seguimiento = cobranza.fecha_seguimiento.date() if cobranza.fecha_seguimiento else None
+      fecha_promesa_pago = cobranza.fecha_promesa_pago
+
+      if fecha_promesa_pago:
+         if dia < fecha_promesa_pago:
+            print(f'Cobranza: {cobranza}: Asesor:{cobranza.asesor_credito}\nFecha Seguimiento: {fecha_seguimiento}, Fecha Promesa de Pago: {fecha_promesa_pago}. Comprobar: {fecha_promesa_pago < dia if fecha_promesa_pago else None}')
+            cobranza.estado_cobranza = 'PENDIENTE'
+            cobranza.resultado = 'Promesa de pago'
+            cobranza.save()
    
-         
-   
-
-            
-
-
-   
-
-   
-
-
-        
-
-    
-    
-    
-
-    
+  
