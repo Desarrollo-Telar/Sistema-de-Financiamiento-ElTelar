deac1a66af717db7236997a152abcd763f7014a0
choc1403 <eloicx@gmail.com>
2026-01-08 16:07:10 -0600

    Cierre diario

--- Backend/project/apps/financings/signals/desembolso.py
+++ Backend/project/apps/financings/signals/desembolso.py
@@ -83,7 +83,7 @@ def verificar_montos_desembolsados(sender, instance,created, **kwargs):
     print(total_desembolso)
     
 
-    monto_pendiente_desembolsar = total_desembolso.monto_total_desembolso if total_desembolso.monto_total_desembolso else 0
+    """monto_pendiente_desembolsar = total_desembolso.monto_total_desembolso if total_desembolso.monto_total_desembolso else 0
     gastos =  instance.total_gastos
 
     # Incluir el monto del desembolso actual
@@ -97,7 +97,7 @@ def verificar_montos_desembolsados(sender, instance,created, **kwargs):
     
     if total_desembolso == total_credito:
         # Indicar que ya se desembolsó completamente
-        credito_desembolsado(instance)
+        credito_desembolsado(instance)"""
 
     # Registrar información adicional
     logger.info(f'Total desembolsado: {total_desembolso}, Total crédito: {total_credito}')
