08505a7ce5d4c0be53ff05f4532b1d33e9a24b6c
choc1403 <eloicx@gmail.com>
2026-01-16 11:23:20 -0600

    Serializador de las Sucursales

--- Backend/project/apps/subsidiaries/api/seriealizer.py
+++ Backend/project/apps/subsidiaries/api/seriealizer.py
@@ -9,3 +9,19 @@ class SubsidiarySerializer(serializers.ModelSerializer):
     class Meta:
         model = Subsidiary
         fields = '__all__'
+
+    def to_representation(self, instance):
+        return {
+            'id':instance.id,
+            'codigo_sucursal':instance.codigo_sucursal,
+            'nombre':instance.nombre,
+            'fecha_apertura':instance.fecha_apertura,
+            'numero_telefono': instance.numero_telefono,
+            'otro_numero_telefono': instance.otro_numero_telefono,
+            'activa':instance.activa,
+            'codigo_postal':instance.codigo_postal,
+            'descripcion':instance.descripcion,
+            'codigo_establecimiento': instance.codigo_establecimiento,
+            'numero_de_cuenta_banco': instance.numero_de_cuenta_banco,
+            'nombre_banco': instance.nombre_banco
+        }
