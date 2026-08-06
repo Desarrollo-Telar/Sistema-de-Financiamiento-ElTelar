3e7e8062d36bd500656e6953bd3d8dd663ef1527
choc1403 <eloicx@gmail.com>
2026-08-06 16:52:34 -0600

    Paginacion

--- Backend/project/project/pagination.py
+++ Backend/project/project/pagination.py
@@ -1,9 +1,13 @@
-# DJANGO
 from django.core.paginator import Paginator
 
-# PAGINACION
-def paginacion(request,list):
+def paginacion(request, list):
     paginator = Paginator(list, 50)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
+    
+    # Inyectamos el rango recortado directamente dentro de page_obj
+    page_obj.custom_page_range = paginator.get_elided_page_range(
+        page_obj.number, on_each_side=2, on_ends=1
+    )
+    
     return page_obj
