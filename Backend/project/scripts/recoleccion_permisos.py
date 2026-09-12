fc85d974b8f70e66d4bb9ae6e8fee38de6c00b2f
choc1403 <eloicx@gmail.com>
2026-09-11 19:50:19 -0600

    Optimizacion de servidor 01

--- Backend/project/scripts/recoleccion_permisos.py
+++ Backend/project/scripts/recoleccion_permisos.py
@@ -2,14 +2,21 @@
 from apps.users.models import PermisoUsuario
 
 def recorrer_los_permisos_usuario(request):
-    permisos_tiene = {}
-    es_autenticado = request.user.is_authenticated
+   
 
-    if not es_autenticado:
-        return None
+    if not request.user.is_authenticated:
+        return {
+            'permisos_usuario': {}
+        }
 
-    for permisos_otorgados in PermisoUsuario.objects.filter(user=request.user):
-        nombre = permisos_otorgados.permiso.codigo_permiso
-        permisos_tiene[f'{nombre}'] = True
-    
-    return permisos_tiene
+    permisos = {
+        codigo: True
+        for codigo in PermisoUsuario.objects.filter(
+            user=request.user
+        ).values_list(
+            'permiso__codigo_permiso',
+            flat=True
+        )
+    }
+
+    return permisos
